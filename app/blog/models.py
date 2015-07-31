adb933d4373a01b8812cced0bf6b1ffb5fdb8629
Miguel Dorta <dortamiguel@gmail.com>
2015-07-31 18:32:35 +0100

    code highlighting aded

--- app/blog/models.py
+++ app/blog/models.py
@@ -63,7 +63,8 @@ class Entry(models.Model):
 
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.title.replace(' ', '_'))
-		self.body_html = markdown(self.body, safe_mode=True, extensions=[CustomVideoExtension()])
+		# self.body_html = markdown(self.body, safe_mode=True, extensions=[CustomVideoExtension()])
+		self.body_html = markdown(self.body, safe_mode=True, extensions=[CustomVideoExtension(), 'codehilite'], extension_configs={ 'codehilite': { 'linenums': 'False', 'css_class': 'highlight', }, })
 		super(Entry, self).save(*args, **kwargs)
 
 	@models.permalink
