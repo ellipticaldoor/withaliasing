1268607a8a9901041a0bbf43bece65b630bcd348
Miguel Dorta <dortamiguel@gmail.com>
2015-08-08 00:50:02 +0100

    minor improvements

--- app/withaliasing/settings/base.py
+++ app/withaliasing/settings/base.py
@@ -55,22 +55,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'withaliasing.urls'
 
-# TEMPLATES = [
-# 	{
-# 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-# 		'DIRS': [],
-# 		'APP_DIRS': True,
-# 		'OPTIONS': {
-# 			'context_processors': [
-# 				'django.template.context_processors.debug',
-# 				'django.template.context_processors.request',
-# 				'django.contrib.auth.context_processors.auth',
-# 				'django.contrib.messages.context_processors.messages',
-# 			],
-# 		},
-# 	},
-# ]
-
 WSGI_APPLICATION = 'withaliasing.wsgi.application'
 
 DATABASES = {
--- app/withaliasing/settings/development.py
+++ app/withaliasing/settings/development.py
@@ -2,16 +2,16 @@ from withaliasing.settings.base import *
 
 DEBUG = True
 
-INSTALLED_APPS += (
-	'debug_toolbar',
-)
-
-MIDDLEWARE_CLASSES += (
-	'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_CONFIG = {
-	'JQUERY_URL': '/s/js/lib/jquery_2.1.3.min.js',
-}
+# INSTALLED_APPS += (
+# 	'debug_toolbar',
+# )
+#
+# MIDDLEWARE_CLASSES += (
+# 	'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
+#
+# DEBUG_TOOLBAR_CONFIG = {
+# 	'JQUERY_URL': '/s/js/lib/jquery_2.1.3.min.js',
+# }
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
