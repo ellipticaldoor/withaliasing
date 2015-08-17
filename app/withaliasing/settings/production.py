d7ff0443e8d256d3677b2e9fa96df5f3723c9b71
Miguel Dorta <dortamiguel@gmail.com>
2015-08-17 02:16:33 +0100

    removed cache config for production

--- app/withaliasing/settings/production.py
+++ app/withaliasing/settings/production.py
@@ -2,11 +2,11 @@ from withaliasing.settings.base import *
 
 DEBUG = False
 
-CACHES = {
-	'default': {
-		'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-		'LOCATION': '127.0.0.1:11211',
-	}
-}
+# CACHES = {
+# 	'default': {
+# 		'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+# 		'LOCATION': '127.0.0.1:11211',
+# 	}
+# }
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
